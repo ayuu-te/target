ec9d490d5fd40c7359bd4f27b5730b4456bba0b9
Ayush Verma <ayushverma.8509@gmail.com>
2025-09-01 23:44:55 +0530

    email

--- qrprint.py
+++ qrprint.py
@@ -1,3 +1,10 @@
-import qrcode as qr
-img = qr.make("https://www.udemy.com/course/full-stack-ai-with-python/learn/lecture/52004391#overview")
+import qrcode
+from PIL import Image
+# img = qrcode.make("https://www.udemy.com/course/full-stack-ai-with-python/learn/lecture/52004391#overview")
+# img.save("qrcode.png")
+qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4)
+
+qr.add_data("https://www.udemy.com/course/full-stack-ai-with-python/learn/lecture/52004391#overview")
+qr.make(fit=True)
+img = qr.make_image(fill_color="red",back_color="white")
 img.save("qrcode.png")
